492452fc8730d76904396cc16e6fd11ecd254b63
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-25 23:20:17 +0300

    generated class

--- YouTubeChannel.cpp
+++ YouTubeChannel.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
-
+#include<list>
 using namespace std;
+
+class YouTubeChennel {
+    string Name;
+    string ownerName;
+    int SubscribersCount;
+    list<string>PublishedVideoTitels;
+
+};
 
 int main()
 {
