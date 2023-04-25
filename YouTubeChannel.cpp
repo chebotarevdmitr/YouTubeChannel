7456524ece576cd5e8155b5bc824157158798e90
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-26 00:35:26 +0300

    object

--- YouTubeChannel.cpp
+++ YouTubeChannel.cpp
@@ -15,9 +15,18 @@ int main()
 {
     YouTubeChennel ytChennal;
     ytChennal.Name = "namechennel";
-    ytChennal.ownerName = "Сat";
+    ytChennal.ownerName = "Cat";
     ytChennal.SubscribersCount = 2000;
     ytChennal.PublishedVideoTitles = { "C++ for beginners Video 1","HTML & CSS Video 1","C++ OOP Video" };
-    system("pause<0");
+
+    cout << "Name:" << ytChennal.Name << endl;
+    cout << "ownerName:" << ytChennal.ownerName << endl;
+    cout << "SubscribersCount " << ytChennal.SubscribersCount << endl;
+    cout << "Videos:" << endl;
+    for (string VideoTitles : ytChennal.PublishedVideoTitles) {
+        cout << VideoTitles << endl;
+    }
+
+    system("pause>0");
 }
 
